72c0e649d28564b0d4518792e40accb7bb5e615e
Paulo Henrique <paulo.junior@laura-br.com>
2020-04-04 18:46:44 -0300

    Detect Route Change

---
+++ Javascript/React.js/detect-route-change.js
@@ -0,0 +1,20 @@
+/*
+Detect Route Change
+For many times, you will find a needs to detect route change and do something (i.e., change the state “loading” into false whenever there is a page refresh or route change)
+To detect route change in React, you need to use listen method in history property
+For example,
+I want to change my state expanded to false whenever there is a route change
+> listen method in history property
+*/
+constructor(props) {
+    super(props);
+    this.props.history.listen((location, action) => {
+        // console.log('history changed')
+        this.routeChange();
+    })
+}
+// routeChange function
+routeChange = () => {
+    const { BaseActions } = this.props
+    BaseActions.sideBar({ expanded: false })
+}
