7875a7f897cdbfb14f3065e0418399118f28ca43
opauloh <paulo.scape@gmail.com>
2020-05-18 03:02:05 -0300

    Scope Chain - add Example

--- Javascript/Concepts/ScopeChain.js
+++ Javascript/Concepts/ScopeChain.js
@@ -7,4 +7,19 @@ colored background. Visually you can see that any child Execution Context can
 reference any variables located in any of its parent Execution Contexts,
 but not vice versa.
 
+
+var name = 'Tyler'
+
+function logName () {
+  console.log(name)
+}
+
+logName()
+
+
+What happens is if the JavaScript engine can’t find a variable local to the
+function’s Execution Context, it’ll look to nearest parent Execution Context for
+that variable. This lookup chain will continue all the way until the engine
+reaches the Global Execution Context. In that case, if the Global Execution
+Context doesn’t have the variable, it’ll throw a Reference Error.
 */
