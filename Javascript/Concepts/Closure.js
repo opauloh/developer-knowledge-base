f04780770ce2e53d99c3efc1cd36a59990a03fc4
opauloh <paulo.scape@gmail.com>
2020-05-18 02:59:24 -0300

    Closure - add Example

--- Javascript/Concepts/Closure.js
+++ Javascript/Concepts/Closure.js
@@ -13,4 +13,15 @@ the x variable (via the Scope Chain).
 As you probably guessed, this concept of a child function “closing” over the
 variable environment of its parent function is called Closures.
 
+var count = 0
+
+function makeAdder(x) {
+  return function inner (y) {
+    return x + y;
+  };
+}
+
+var add5 = makeAdder(5);
+count += add5(2)
+
 */
