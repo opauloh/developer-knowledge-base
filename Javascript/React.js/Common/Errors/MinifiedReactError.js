a3950e95012106d3e4fc3dcb41a334f3c60f6f0f
opauloh <paulo.scape@gmail.com>
2020-05-21 16:08:37 -0300

    React.js - Errors - Minified React Error

---
+++ Javascript/React.js/Common/Errors/MinifiedReactError.js
@@ -0,0 +1,19 @@
+/*
+In the minified production build of React, we avoid sending down full error messages in order to reduce the number of
+ bytes sent over the wire.
+
+We highly recommend using the development build locally when debugging your app since it tracks additional debug info
+ and provides helpful warnings about potential problems in your apps, but if you encounter an exception while using the
+  production build, this page will reassemble the original text of the error.
+
+The full text of the error you just encountered is:
+
+ni(...): Nothing was returned from render. This usually means a return statement is missing. Or, to render nothing,
+return null.
+
+
+
+Possible solutions: you return nothing as result of rendermethod of you react component.
+Return something else instead of it.
+
+*/
