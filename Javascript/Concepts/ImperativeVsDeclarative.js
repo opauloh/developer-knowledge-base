c1f8f4805a3e63f93285f078568cfb4686034f9b
opauloh <paulo.scape@gmail.com>
2020-05-24 14:09:26 -0300

    Imperative vs Declarative programming

---
+++ Javascript/Concepts/ImperativeVsDeclarative.js
@@ -0,0 +1,8 @@
+/*
+Sometimes We’re conforming to the
+operational model of the machine by describing how we want to accomplish the
+task. Instead, what if we took a more declarative approach ? Instead of
+describing how we want to accomplish the task, let’s describe what we’re trying
+to accomplish.This declarative approach will allow us to conform closer to the
+mental model of the developer.
+*/
