f91af361ecc1b3ac269b09390c4c8b3dc482676c
Paulo Henrique <paulo.scape@gmail.com>
2024-03-30 19:28:07 -0700

    getting all object values

--- Typescript/Tips.ts
+++ Typescript/Tips.ts
@@ -56,3 +56,20 @@ type AlternativeIndividualProgramInline = (typeof programModeEnumMap)[Exclude<
   keyof typeof programModeEnumMap,
   'GROUP' | 'ANNOUNCEMENT'
 >];
+
+/*
+Use Two Operators With Indexed Access to Get All of an Object's Values
+*/
+
+const frontendToBackendEnumMap = {
+  singleModule: 'SINGLE_MODULE',
+  multiModule: 'MULTI_MODULE',
+  sharedModule: 'SHARED_MODULE',
+} as const;
+
+type BackendModuleEnum = (typeof frontendToBackendEnumMap)[keyof typeof frontendToBackendEnumMap];
+
+// more readable way:
+type FrontentToBackend = typeof frontendToBackendEnumMap;
+type BackendModuleEnumReadable = FrontentToBackend[keyof FrontentToBackend];
+
