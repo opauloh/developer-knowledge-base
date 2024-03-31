f5b9799801897e98955249782319f4c6a8488cc1
Paulo Henrique <paulo.scape@gmail.com>
2024-03-30 19:27:12 -0700

    how to create a union from object values

--- Typescript/Tips.ts
+++ Typescript/Tips.ts
@@ -23,3 +23,23 @@ interface Props {
 }
 
 const MovieCard: React.FC<Props> = ({ id, title, poster }) => ({});
+
+/*
+How to Create a Union From an Object's values:
+*/
+
+const programModeEnumMap = {
+  GROUP: 'group',
+  ANNOUNCEMENT: 'announcement',
+  ONE_ON_ONE: '1on1',
+  SELF_DIRECTED: 'selfDirected',
+  PLANNED_ONE_ON_ONE: 'planned1on1',
+  PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
+} as const;
+
+type IndividualProgram = (typeof programModeEnumMap)[
+  | 'ONE_ON_ONE'
+  | 'SELF_DIRECTED'
+  | 'PLANNED_ONE_ON_ONE'
+  | 'PLANNED_SELF_DIRECTED']; // '1on1' | 'selfDirected' | 'planned1on1' | 'plannedSelfDirected'
+
